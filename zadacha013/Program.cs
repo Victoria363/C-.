0847600aa3e2274a7277da36ba96d3de49e24583
Viktoria <luchovitsy@yandex.ru>
2023-05-14 22:53:59 +0300

    zadaha 13

--- zadacha013/Program.cs
+++ zadacha013/Program.cs
@@ -1,11 +1,25 @@
-﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите число");
-int num = int.Parse(Console.ReadLine());
-int p=100;
-int os=10;
-int m = num%os;
-
+Console.WriteLine ("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+int step = 1;
+int checkstep = number;
+while (checkstep > 9)
+{
+checkstep = checkstep / 10;
+step = step + 1;
+}
+if (step < 3)
+{
+Console.WriteLine("Введенное вами число имеет меньше трех цифр, повторите ввод: ");
+}
+else
+{
+while (step > 3)
+{
+number = number/10;
+step = step - 1;
+}
+number = number%10;
+Console.WriteLine ("Третья цифра числа - " +number);
+}
